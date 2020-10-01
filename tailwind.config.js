a77f57a034e419d51a6c67ad67e1de82dc781602
SamrithaS <samritha@timeless.co>
2020-10-01 11:55:03 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      boxShadow: {
+        xs: "inset 0px -1px 0px #E3E9ED"
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
