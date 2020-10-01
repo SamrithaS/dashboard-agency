ed5924aa501d368724303caf9a379998a505bc21
SamrithaS <samritha@timeless.co>
2020-10-01 19:04:49 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,23 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
   theme: {
-    extend: {
-      boxShadow: {
-        xs: "inset 0px -1px 0px #E3E9ED"
-      }
+    boxShadow: {
+      g: "0px 0.5px 2px rgba(0, 0, 0, 0.3), 0px 2px 4px rgba(0, 0, 0, 0.04)",
+      xs: "inset 0px -1px 0px #E3E9ED"
+    },
+    screens: {
+      xs: { max: "520px" },
+      xl: { max: "1279px" },
+
+      lg: { max: "1023px" },
+
+      md: { max: "767px" },
+      sm: { max: "620px" }
     }
   },
-  variants: {},
+
+  variants: {
+    backgroundPosition: ["responsive"],
+    backgroundPosition: ["responsive", "hover", "focus"]
+  },
   plugins: []
 };
